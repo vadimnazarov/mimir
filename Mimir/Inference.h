f1fc3781339ea1225bb73720addf1e35ae6ab287
Vadim Nazarov <vdn@mailbox.org>
2016-03-04 14:01:39 +0300

    add headers Model and Inference

---
+++ Mimir/Inference.h
@@ -0,0 +1,8 @@
+//
+// Created by Vadim N. on 04/03/2016.
+//
+
+#ifndef MIMIR_INFERENCE_H
+#define MIMIR_INFERENCE_H
+
+#endif //MIMIR_INFERENCE_H
---
+++ Mimir/Model.h
@@ -0,0 +1,11 @@
+//
+// Created by Vadim N. on 04/03/2016.
+//
+
+#ifndef MIMIR_MODEL_H
+#define MIMIR_MODEL_H
+
+
+
+
+#endif //MIMIR_MODEL_H
