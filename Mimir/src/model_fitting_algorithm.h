b6417fed3b3c9cb4f537ff9dc21ab1801254ccf2
Vadim Nazarov <vdn@mailbox.org>
2016-03-04 14:01:28 +0300

    add ModelFittingAlgorithm

---
+++ Mimir/src/model_fitting_algorithm.h
@@ -0,0 +1,46 @@
+//
+// Created by Vadim N. on 04/03/2016.
+//
+
+#ifndef MIMIR_MODEL_FITTING_ALGORITHM_H
+#define MIMIR_MODEL_FITTING_ALGORITHM_H
+
+
+#include "abstract_selection_model.h"
+
+#include "Ymir/Model"
+
+using namespace ymir;
+
+
+namespace mimir {
+
+    template <AbstractSelectionModel M>
+    class ModelFittingAlgorithm {
+    public:
+
+        struct Parameters {
+            /* eps, num_iterations, etc. */
+        };
+
+
+        virtual void fit(M *model,
+                         const Cloneset &exp_data,
+                         const Cloneset &gen_data,
+                         const Parameters &params) const = 0;
+
+        void fit(M *model,
+                 const Cloneset &exp_data,
+                 const ProbabilisticAssemblingModel &model,
+                 size_t num_clonotypes_to_generate = 1000000,
+                 const Parameters &params) const
+        {
+            Cloneset gen_data = model.generateSequences(num_clonotypes_to_generate);
+            this->fit(M, exp_data, gen_data, params);
+        }
+    };
+
+}
+
+
+#endif //MIMIR_MODEL_FITTING_ALGORITHM_H
